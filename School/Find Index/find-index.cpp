b07ab71d9b3b53d6b0f069386c0cc6e701ce815d
rohit141914 <115409470+rohit141914@users.noreply.github.com>
2024-01-24 13:31:52 +0530

    Added Solution - GfG to GitHub

---
+++ School/Find Index/find-index.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <bits/stdc++.h> 
+#include <vector> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    vector<int> findIndex(int a[], int n, int key)
+    {
+        //code here.
+        int l=-1;
+        int r=-1;
+        for(int i=0;i<n;i++){
+            if(l==-1 && a[i]==key){
+                l=i;
+                r=i;
+            }
+            else if(a[i]==key){
+                r=i;
+            }
+        }
+        vector<int> ans;
+        ans.push_back(l);
+        ans.push_back(r);
+        return ans;
+    }
+  
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        vector<int> res;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        int key;
+        cin>>key;
+        Solution ob;
+        res=ob.findIndex(arr, n, key);
+        for (int i = 0; i < res.size(); i++) 
+        cout << res[i] << " ";
+        cout << "\n";
+    }
+    
+    return 0;
+}
+
+// } Driver Code Ends
