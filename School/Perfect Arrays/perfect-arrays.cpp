9b96b7ff40b148f08503ca6e004a41fc8fc3ebca
rohit141914 <115409470+rohit141914@users.noreply.github.com>
2024-01-29 16:42:47 +0530

    Added Solution - GfG to GitHub

---
+++ School/Perfect Arrays/perfect-arrays.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    bool IsPerfect(int a[],int n)
+    {
+        // Complete the function
+        int s=0;
+        int e=n-1;
+        while(s<=e){
+            if(a[s]!=a[e]){
+                return 0;
+            }
+            s++;
+            e--;
+        }
+        return 1;
+    }
+};
+
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;cin>>t;
+    while(t--)
+    {
+        int n;cin>>n;
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        Solution ob;
+        if(ob.IsPerfect(a,n))
+            cout<<"PERFECT\n";
+        else
+            cout<<"NOT PERFECT\n";
+        
+    }
+    
+}
+// } Driver Code Ends
