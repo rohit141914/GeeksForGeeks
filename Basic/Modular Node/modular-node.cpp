80665950694541ef8d5fa2deb771dfcec9ce4aca
rohit141914 <115409470+rohit141914@users.noreply.github.com>
2024-02-06 10:55:02 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Modular Node/modular-node.cpp
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+void append(struct Node** headRef, int newData)
+{
+	struct Node* new_node = new Node(newData);
+	struct Node *last = *headRef;
+	if (*headRef == NULL)
+	{
+	    *headRef = new_node;
+	    return;
+	}
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new_node;
+	return;
+}
+
+int modularNode(Node* head, int k);
+
+// Driver program to test above
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        struct Node* head = NULL;
+        int n, tmp, k;
+        cin>>n;
+        while(n--){
+            cin>>tmp;
+            append(&head, tmp);
+        }
+        cin>>k;
+        cout<<modularNode(head, k)<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/*Struture of the node of the linked list is as:
+
+struct Node {
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+*/
+// Your task is to complete the function
+// function should return the modular Node
+// if no such node is present then return -1
+int modularNode(Node* head, int k)
+{
+	// Code hereint res=0
+	int res=-1;
+	int p=1;
+	while(head!=NULL){
+	   // p++;
+	    if(p==k){
+	        res=head->data;
+	       // cout<<p<<"-";
+	        p=0;
+	    }
+	    p++;
+	    head=head->next;
+	}
+	return res;
+}
