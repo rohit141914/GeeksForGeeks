7aa0c265387c6b87d339d61c3800ef1803eaf3e1
rohit141914 <115409470+rohit141914@users.noreply.github.com>
2024-02-17 23:40:28 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Queue Reversal/queue-reversal.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//function Template for C++
+
+//Function to reverse the queue.
+class Solution
+{
+    public:
+    queue<int> rev(queue<int> q)
+    {
+        // add code here.
+        stack<int> tp;
+        while(!q.empty()){
+            tp.push(q.front());
+            q.pop();
+        }
+        while(!tp.empty()){
+            q.push(tp.top());
+            tp.pop();
+        }
+        return q;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int test;
+    cin>>test; 
+    while(test--)
+    {
+    queue<int> q; 
+    int n, var; 
+    cin>>n; 
+    while(n--)
+    {
+        cin>>var; 
+        q.push(var);
+    }
+    Solution ob;
+    queue<int> a=ob.rev(q); 
+    while(!a.empty())
+    {
+        cout<<a.front()<<" ";
+        a.pop();
+    }
+    cout<<endl; 
+    }
+}
+// } Driver Code Ends
